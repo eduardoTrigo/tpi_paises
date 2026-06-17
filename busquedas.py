d54fe06b1372c976e4dcfe04170ce89346bf21e5
Darofhaz22 <todorock22@gmail.com>
2026-06-17 18:21:55 -0300

    arreglos opcionales
    arreglos de opcionales

--- busquedas.py
+++ busquedas.py
@@ -1,12 +1,19 @@
 from datos import mostrar_lista_paises
 
-
 def buscar_pais_por_nombre(paises):
-    nombre_buscado = input("Ingrese el nombre o parte del nombre del país: ").strip().lower()
 
-    if not nombre_buscado:
-        print("Debe ingresar un texto para buscar.")
-        return
+    while True:
+        nombre_buscado = input("Ingrese el nombre o parte del nombre del país (0 para cancelar): ").strip().lower()
+        
+        if nombre_buscado == "0":
+            return
+        if not nombre_buscado:
+            print("Debe ingresar un texto para buscar.")
+            continue
+        if not nombre_buscado.replace(" ", "").isalpha():
+            print("Error: solo se permiten letras.")
+            continue
+        break
 
     resultados = []
 
